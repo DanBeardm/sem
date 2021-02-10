e746eda6dea12ea3766ac42a8f9165d2354516c8
Dan Beardmore <40435226@live.napier.ac.uk>
2021-02-10 11:51:45 +0000

    Adding App and Dockerfile

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,12 @@
+package com.napier.sem;
+/**
+ * Program sends hello
+ * world to display
+ **/
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
